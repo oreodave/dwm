b390f3059e7db58069c2fe778455808740a0f0c4
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-09-29 14:02:18 +0100

    ~scratchpad command to Emacs

--- config.h
+++ config.h
@@ -42,7 +42,6 @@ static const Rule rules[] = {
 	 */
 	/* class            instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
-	{ "Emacs",          NULL,       NULL,       GTMask(1),    0,           -1 },
 	{ "qutebrowser",    NULL,       NULL,       GTMask(2),    0,           -1 },
 	{ "mpv",            NULL,       NULL,       GTMask(3),    0,           -1 },
 	{ "media-term",     NULL,       NULL,       GTMask(3),    0,           -1 },
@@ -80,7 +79,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "emacs", "-T", scratchpadname, NULL };
 
 static char *statuscmds[] = { "notify-send Mouse$BUTTON" };
 static char *statuscmd[] = { "/bin/sh", "-c", NULL, NULL };
