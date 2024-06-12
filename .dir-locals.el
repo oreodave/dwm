67a1f67487d4bc0e992892c754657be44ad1cd6b
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-06-12 04:31:49 +0100

    Added compile-command dir-local for quick compilation

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c-mode . ((indent-tabs-mode . t)
-            (flycheck-mode . -1))))
+            (flycheck-mode    . -1)))
+ (nil    . ((compile-command  . "make && sudo make install && killall dwm"))))
