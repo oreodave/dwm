21d9094b456316e4db4af4236048b81a0a88a08a
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-11-01 06:30:01 +0000

    Remove old entries from rules array.

--- config.h
+++ config.h
@@ -47,16 +47,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                             instance        title    tags mask     isfloating   monitor */
-	{ "Gimp",                            NULL,           NULL,    0,            1,           -1 },
-	{ "mpv",                             NULL,           NULL,    GTMask(3),    0,           -1 },
-	{ "Onboard",                         NULL,           NULL,    0,            0,           -1 },
-	{ "qutebrowser",                     NULL,           NULL,    GTMask(2),    0,           -1 },
-	{ "firefox",                         NULL,           NULL,    GTMask(2),    0,           -1 },
-	{ "Chromium",                        NULL,           NULL,    GTMask(2),    0,           -1 },
-	{ "Spotify",                         NULL,           NULL,    GTMask(3),    0,           -1 },
-	{ "Zathura",                         NULL,           NULL,    GTMask(4),    0,           -1 },
-	{ "Com.github.xournalpp.xournalpp",  NULL,           NULL,    GTMask(5),    0,           -1 },
+	/* class         instance   title   tags mask     isfloating   monitor */
+	{ "Gimp",        NULL,      NULL,   0,            1,           -1 },
+	{ "mpv",         NULL,      NULL,   GTMask(3),    0,           -1 },
+	{ "Onboard",     NULL,      NULL,   0,            0,           -1 },
+	{ "firefox",     NULL,      NULL,   GTMask(2),    0,           -1 },
+	{ "Chromium",    NULL,      NULL,   GTMask(2),    0,           -1 },
+	{ "Spotify",     NULL,      NULL,   GTMask(3),    0,           -1 },
+	{ "Zathura",     NULL,      NULL,   GTMask(4),    0,           -1 },
 };
 
 /* layout(s) */
